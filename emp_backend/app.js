c7c07eaf4a474b7c127722c596d05dd7541e203c
Sourab19 <sourabberlin007@gmail.com>
2025-04-11 12:05:12 +0530

    update cors for all client request

--- emp_backend/app.js
+++ emp_backend/app.js
@@ -15,7 +15,13 @@ app.use(
     allowedHeaders: ['Content-Type', 'Authorization','token'],
   })
 );
-// app.use(cors());
+
+app.options("*", cors({
+  origin: ['https://employ-deploy-client.vercel.app'],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "token"],
+}));
 
 
 
